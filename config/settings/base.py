e4fe83509d7e164238de8698eb7f9550807f9556
vladkha <vladislavkhaitov@gmail.com>
2017-10-30 21:41:33 +0200

    Move whitenoise setting to base

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ DJANGO_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
 )
 
@@ -53,6 +54,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -60,6 +62,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'config.urls'
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -2,5 +2,3 @@ from .base import *
 
 ALLOWED_HOSTS = ['*']
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-
-MIDDLEWARE = [MIDDLEWARE[0]] + ['whitenoise.middleware.WhiteNoiseMiddleware'] + MIDDLEWARE[1:]
