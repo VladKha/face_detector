e88d42383bf99ae341e3e8a8dbe9c7d1d93ae196
vladkha <vladislavkhaitov@gmail.com>
2017-11-01 22:53:00 +0200

    Update celery environment settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,5 +3,5 @@ from .base import *
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='development_key')
 
-BROKER_URL = env.str('CLOUDAMQP_URL', default='amqp://guest:guest@localhost//')
-CELERY_RESULT_BACKEND = env.str('CLOUDAMQP_URL', default='amqp://guest:guest@localhost//')
+BROKER_URL = env('CLOUDAMQP_URL', default='amqp://guest:guest@localhost//')
+CELERY_RESULT_BACKEND = env('CLOUDAMQP_URL', default='amqp://guest:guest@localhost//')
