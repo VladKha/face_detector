9897171dbc1d3e8863a7da05b9bede0f8a2d2518
vladkha <vladislavkhaitov@gmail.com>
2017-12-26 14:36:13 +0200

    Cleanup celery config

--- config/celery.py
+++ config/celery.py
@@ -1,11 +1,5 @@
-import os
-
 from celery import Celery
 from django.conf import settings
-
-if not settings.configured:
-    # set the default Django settings module for the 'celery' program.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')  # pragma: no cover
 
 app = Celery('face_detector')
 
